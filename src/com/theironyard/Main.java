1f3198d729d6718d70688c706f4c8eb3ff352de5
earlbozarth <ehbbozarth@gmail.com>
2015-11-02 15:10:01 -0500

    BeerTracker Database (Incomplete w/ ROWNUM)

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -4,13 +4,16 @@ import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
-
-import java.beans.*;
 import java.sql.*;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
+/*
+ROWNUM does not work fully
+Will address issue next day in class i.e. November 3rd, 2015
+ */
 public class Main {
 
     public static void main(String[] args) throws SQLException {
@@ -18,8 +21,6 @@ public class Main {
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
         Statement stmt = conn.createStatement();
         stmt.execute("CREATE TABLE IF NOT EXISTS beers (name VARCHAR, type VARCHAR)");
-
-
 
         Spark.get(
                 "/",
@@ -36,7 +37,8 @@ public class Main {
                     return new ModelAndView(m, "logged-in.html");
                 }),
                 new MustacheTemplateEngine()
-        );
+        );//End of Spark.get() "/"
+
         Spark.post(
                 "/login",
                 ((request, response) -> {
@@ -46,7 +48,8 @@ public class Main {
                     response.redirect("/");
                     return "";
                 })
-        );
+        );//End of Spark.post() "/login"
+
         Spark.post(
                 "/create-beer",
                 ((request, response) -> {
@@ -57,7 +60,8 @@ public class Main {
                     response.redirect("/");
                     return "";
                 })
-        );
+        );//End of Spark.post() "/create-beer
+
         Spark.post(
                 "/delete-beer",
                 ((request, response) -> {
@@ -71,7 +75,7 @@ public class Main {
                     response.redirect("/");
                     return "";
                 })
-        );
+        );//End of Spark.post() "/delete-beer"
 
         Spark.post(
                 "/edit-beer",
@@ -84,14 +88,14 @@ public class Main {
                         int beerNum = Integer.valueOf(editBeer);
                         updateBeer(conn,beerName, beerType, beerNum );
                         stmt2.execute();
-
                     } catch (Exception e){
 
                     }
                     response.redirect("/");
                     return "";
                 })
-        );
+        );//End of Spark.post() "/edit-beer"
+
     }//End of Main Method
 
     static void insertBeer(Connection conn, String beerName, String beerType) throws SQLException {
@@ -99,13 +103,13 @@ public class Main {
         stmt.setString(1, beerName);
         stmt.setString(2, beerType);
         stmt.execute();
-    }
+    }//End of insertBeer Method
 
     static void deleteBeer(Connection conn, int selectNum) throws SQLException {
         PreparedStatement stmt = conn.prepareStatement("DELETE from players WHERE ROWNUM = ?");
         stmt.setInt(1, selectNum);
         stmt.execute();
-    }
+    }//End of deleteBeer Method
 
     static ArrayList<Beer> selectBeer(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
@@ -120,7 +124,7 @@ public class Main {
             tempID++;
         }
         return beerArrayList;
-    }
+    }//End of selectBeer Method
 
     static void updateBeer(Connection conn, String beerName, String beerType, int selectNum) throws SQLException {
         PreparedStatement stmt = conn.prepareStatement("UPDATE beers SET name = ? AND type = ? WHERE ROWNUM = ?");
@@ -128,7 +132,6 @@ public class Main {
         stmt.setString(2, beerType);
         stmt.setInt(3, selectNum);
         stmt.execute();
-    }
-
+    }//End of updateBeer Method
 
 }//End of Main Class
